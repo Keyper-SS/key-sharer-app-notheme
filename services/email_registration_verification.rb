d90aaa2e64ed3b452a17638aefa434ef6ae58fe2
vicky <lch82327@gmail.com>
2016-05-18 20:21:11 +0800

    change email content

--- services/email_registration_verification.rb
+++ services/email_registration_verification.rb
@@ -16,7 +16,7 @@ class EmailRegistrationVerification
     verification_url = "#{ENV['APP_HOST']}/register/#{token}/verify"
 
     <<~END_EMAIL
-      <H1>ShareConfig Registration Received<H1>
+      <H1>Key Share Registration Received<H1>
       <p>Please <a href=\"#{verification_url}\">click here</a> to validate your
       email. You will be asked to set a password to activate your account.</p>
     END_EMAIL
