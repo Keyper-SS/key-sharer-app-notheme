216774597f9a105c6a17cce60f520918a5c29a2c
yiweihuang <n1993212@gmail.com>
2016-05-27 01:57:58 +0800

    fix init

--- services/init.rb
+++ services/init.rb
@@ -1,3 +1,6 @@
 require_relative 'find_authenticated_user'
 require_relative 'create_verified_user'
 require_relative 'email_registration_verification'
+require_relative 'get_owned_secrets'
+require_relative 'get_received_secrets'
+require_relative 'get_shared_secrets'
